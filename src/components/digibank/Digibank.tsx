f33135ddad59276d6a23377a93b2b58861d4f8a9
Souad5 <souadalkabir@gmail.com>
2025-12-03 17:11:19 +0600

    done

--- src/components/digibank/Digibank.tsx
+++ src/components/digibank/Digibank.tsx
@@ -1,60 +1,61 @@
+
 const Digibank = () => {
   return (
-    <div className="lg:-mt-20 py-20 bg-gray-100">
-      <div className="max-w-11/12 mx-auto">
-        <h1 className="text-3xl font-bold mb-5 text-center md:text-left">
-          Why choose Digibank
-        </h1>
-        <p className="text-gray-600  text-center md:text-left">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
-          Velit nam, neque, error eum quas, fugit culpa fugiat <br />
-          consequatur cupiditate optio .
-        </p>
-      </div>
+      <div className="lg:-mt-20 py-20 bg-gray-100">
+        <div className="max-w-11/12 mx-auto">
+          <h1 className="text-3xl font-bold mb-5 text-center md:text-left">
+            Why choose Digibank
+          </h1>
+          <p className="text-gray-600  text-center md:text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+            Velit nam, neque, error eum quas, fugit culpa fugiat <br />
+            consequatur cupiditate optio .
+          </p>
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center items-center max-w-11/12 mx-auto mt-10">
-        <div className="space-y-5 flex flex-col md:justify-baseline md:items-baseline justify-center items-center">
-          <img src="/icon-online.svg" alt="" />
-          <h1 className="text-2xl font-bold">Online Banking</h1>
-          <p className="text-gray-600 text-center md:text-left ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            dolorem porro sequi quae sapiente dolores vel vero perferendis
-            fugiat optio dignissimos architecto itaque, culpa dolore a saepe
-            cupiditate? Obcaecati, eius!
-          </p>
-        </div>
-        <div className="space-y-5 flex flex-col md:justify-baseline md:items-baseline justify-center items-center">
-          <img src="/icon-budgeting.svg" alt="" />
-          <h1 className="text-2xl font-bold">Simple Budgeting</h1>
-          <p className="text-gray-600 text-center md:text-left">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            dolorem porro sequi quae sapiente dolores vel vero perferendis
-            fugiat optio dignissimos architecto itaque, culpa dolore a saepe
-            cupiditate? Obcaecati, eius!
-          </p>
-        </div>
-        <div className="space-y-5 flex flex-col md:justify-baseline md:items-baseline justify-center items-center">
-          <img src="/icon-onboarding.svg" alt="" />
-          <h1 className="text-2xl font-bold">Fast Onboarding</h1>
-          <p className="text-gray-600 text-center md:text-left">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            dolorem porro sequi quae sapiente dolores vel vero perferendis
-            fugiat optio dignissimos architecto itaque, culpa dolore a saepe
-            cupiditate? Obcaecati, eius!
-          </p>
-        </div>
-        <div className="space-y-5 flex flex-col md:justify-baseline md:items-baseline justify-center items-center">
-          <img src="/icon-api.svg" alt="" />
-          <h1 className="text-2xl font-bold">Open Api</h1>
-          <p className="text-gray-600 text-center md:text-left">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            dolorem porro sequi quae sapiente dolores vel vero perferendis
-            fugiat optio dignissimos architecto itaque, culpa dolore a saepe
-            cupiditate? Obcaecati, eius!
-          </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center items-center max-w-11/12 mx-auto mt-10">
+          <div className="space-y-5 flex flex-col md:justify-baseline md:items-baseline justify-center items-center">
+            <img src="/icon-online.svg" alt="" />
+            <h1 className="text-2xl font-bold">Online Banking</h1>
+            <p className="text-gray-600 text-center md:text-left ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              dolorem porro sequi quae sapiente dolores vel vero perferendis
+              fugiat optio dignissimos architecto itaque, culpa dolore a saepe
+              cupiditate? Obcaecati, eius!
+            </p>
+          </div>
+          <div className="space-y-5 flex flex-col md:justify-baseline md:items-baseline justify-center items-center">
+            <img src="/icon-budgeting.svg" alt="" />
+            <h1 className="text-2xl font-bold">Simple Budgeting</h1>
+            <p className="text-gray-600 text-center md:text-left">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              dolorem porro sequi quae sapiente dolores vel vero perferendis
+              fugiat optio dignissimos architecto itaque, culpa dolore a saepe
+              cupiditate? Obcaecati, eius!
+            </p>
+          </div>
+          <div className="space-y-5 flex flex-col md:justify-baseline md:items-baseline justify-center items-center">
+            <img src="/icon-onboarding.svg" alt="" />
+            <h1 className="text-2xl font-bold">Fast Onboarding</h1>
+            <p className="text-gray-600 text-center md:text-left">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              dolorem porro sequi quae sapiente dolores vel vero perferendis
+              fugiat optio dignissimos architecto itaque, culpa dolore a saepe
+              cupiditate? Obcaecati, eius!
+            </p>
+          </div>
+          <div className="space-y-5 flex flex-col md:justify-baseline md:items-baseline justify-center items-center">
+            <img src="/icon-api.svg" alt="" />
+            <h1 className="text-2xl font-bold">Open Api</h1>
+            <p className="text-gray-600 text-center md:text-left">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              dolorem porro sequi quae sapiente dolores vel vero perferendis
+              fugiat optio dignissimos architecto itaque, culpa dolore a saepe
+              cupiditate? Obcaecati, eius!
+            </p>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,38 +1,42 @@
 import Button from "../../utilis/Button";
-import {motion} from "framer-motion"
-const transition = {
+import { motion } from "framer-motion";
+
+const transition: Record<string, unknown> = {
   duration: 0.8,
   delay: 0.4,
   ease: [0, 0.71, 0.2, 1.01],
 };
+
 const Hero = () => {
-    return (
-      <motion.div
-        animate={{ y: -40 }}
-        transition={transition}
-        className="flex flex-col-reverse md:flex-row justify-between items-center gap-5 -mt-28 overflow-hidden pb-5"
-      >
-        <div className="lg:ml-24 pl-6 space-y-4 flex flex-col lg:justify-baseline justify-center items-center lg:items-baseline">
-          <h1 className="text-3xl font-bold">
-            Next generation <br /> digital banking
-          </h1>
-          <h4 className="text-center md:text-left lg:text text-gray-700">
-            Lorem ipsum dolor sit amet,
-            <br /> consectetur adipisicing elit. Nulla rerum, deleniti eum,
-            <br /> sit tempora illum ut aperiam repellat quod doloribus minus
-            tempore?
-          </h4>
-          <Button />
-        </div>
-        <div className="bg-[url('/bg-intro-desktop.svg')] bg-no-repeat bg-cover lg:relative lg:h-screen w-full lg:w-[1000px] lg:-right-10">
-          <img
-            src="/image-mockups.png"
-            alt=""
-            className="lg:absolute w-auto lg:h-[105%] lg:-right-20 "
-          />
-        </div>
-      </motion.div>
-    );
+  return (
+    <motion.div
+      initial={{ y: 0 }}
+      animate={{ y: -40 }}
+      transition={transition}
+      className="flex flex-col-reverse md:flex-row justify-between items-center gap-5 -mt-28 overflow-hidden pb-5"
+    >
+      <div className="lg:ml-24 pl-6 space-y-4 flex flex-col lg:justify-baseline justify-center items-center lg:items-baseline">
+        <h1 className="text-3xl font-bold">
+          Next generation <br /> digital banking
+        </h1>
+        <h4 className="text-center md:text-left lg:text text-gray-700">
+          Lorem ipsum dolor sit amet,
+          <br /> consectetur adipisicing elit. Nulla rerum, deleniti eum,
+          <br /> sit tempora illum ut aperiam repellat quod doloribus minus
+          tempore?
+        </h4>
+        <Button />
+      </div>
+
+      <div className="bg-[url('/bg-intro-desktop.svg')] bg-no-repeat bg-cover lg:relative lg:h-screen w-full lg:w-[1000px] lg:-right-10">
+        <img
+          src="/image-mockups.png"
+          alt=""
+          className="lg:absolute w-auto lg:h-[105%] lg:-right-20"
+        />
+      </div>
+    </motion.div>
+  );
 };
 
 export default Hero;
